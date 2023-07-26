fa4fb5e6ec40f154b2c9890714d1719e04fd6bbb
V-Perotto <vittorio.perotto@outlook.com>
2023-07-26 01:06:57 -0300

    Modified and created Contato Blueprint

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 from flask import Flask
-from flask_mysql import MySQL
+from controllers.contato_controller import contato
 
-api = Flask(__name__)
-api.config['MYSQL_HOST'] = '<host>'
-api.config["MYSQL_PORT"] = '<port>'
-api.config['MYSQL_USER'] = '<user>'
-api.config['MYSQL_PASSWORD'] = '<password>'
-api.config['MYSQL_DB'] = '<database>'
-
-db = MySQL(api)
+def create_app(host, port, user, pwd, db) -> Flask:
+    
+    app = Flask(__name__)
+    app.config['MYSQL_DATABASE_HOST'] = host
+    app.config["MYSQL_DATABASE_PORT"] = port
+    app.config['MYSQL_DATABASE_USER'] = user
+    app.config['MYSQL_DATABASE_PASSWORD'] = pwd
+    app.config['MYSQL_DATABASE_DB'] = db
+    
+    app.register_blueprint(contato, url_prefix='/contato')
+    
+    return app
